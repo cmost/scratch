14a87152cb6987a719ca823ea9ca4ca6da0cc060
rameshvk <ramesh.vyaghrapuri@gmail.com>
2024-09-11 21:23:49 +0000

    Check for Bluetooth device

--- hrmon.js
+++ hrmon.js
@@ -1,10 +1,27 @@
 (function() {
     window.addEventListener("DOMContentLoaded", () => {
         const log = logger();
+        const bt =  navigator.bluetooth;
 
-        log("initialized!\b");
+        if (!bt) {
+            log("Browser does not support Bluetooth\n");
+            return;
+        }
+
+        monitor(log, bt);
     });
     
+    async function monitor(log, bt) {
+        try {
+            const device = await bt.requestDevice({
+                filters: [{ services: ["heart_rate"]}],
+                optionalServices: ["battery_service"],
+            });
+            log(`Found device ${device.name}\n`);
+        } catch(e) {
+            log("Got an exception:" + e + "\n");
+        }
+    }
     function logger () {
         let el = document.getElementById ("log");
         if (!el) {
